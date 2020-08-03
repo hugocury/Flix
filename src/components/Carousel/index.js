b0102d7b3425a6b37c8807614408d75319d0ef67
unknown <hugocury@gmail.comm>
2020-08-03 20:42:29 -0300

    carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -3,14 +3,12 @@ import { VideoCardGroupContainer, Title, ExtraLink } from './styles';
 import VideoCard from './components/VideoCard';
 import Slider, { SliderItem } from './components/Slider';
 
-function Carousel({
-  ignoreFirstVideo,
-  category,
-}) {
+function Carousel({ ignoreFirstVideo, category }) {
   const categoryTitle = category.titulo;
   const categoryColor = category.cor;
   const categoryExtraLink = category.link_extra;
   const { videos } = category;
+
   return (
     <VideoCardGroupContainer>
       {categoryTitle && (
